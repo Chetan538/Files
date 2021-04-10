eaf3262a77a508c96ba9623c6820053bc9d4da9e
Chetan538 <82130147+Chetan538@users.noreply.github.com>
2021-04-10 12:34:23 +0530

    Add files via upload

---
+++ Controllers/InventoryController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Application.Models;
+namespace Application.Controllers
+{
+    public class InventoryController : ApiController
+    {
+        ProductEntities DC = new ProductEntities();
+        public IHttpActionResult Getstuff()
+        {
+
+            var results = DC.stuffs.ToList();
+            return Ok(results);
+
+
+        }
+        [HttpPost]
+        public IHttpActionResult stuffinsert(stuff stuffInsert)
+        {
+            DC.stuffs.Add(stuffInsert);
+            DC.SaveChanges();
+
+            return Ok();
+
+        }
+
+        public IHttpActionResult Getitemid(int id)
+        {
+            Stuffclass stuffdetails = null;
+            stuffdetails = DC.stuffs.Where(x => x.itemid == id).Select(x => new Stuffclass()
+            {
+                itemid = x.itemid,
+                name = x.name,
+                descriptions = x.descriptions,
+                price = x.price,
+
+
+
+            }).FirstOrDefault<Stuffclass>();
+            if (stuffdetails == null)
+            {
+                return NotFound();
+            }
+            return Ok(stuffdetails);
+
+
+        }
+
+        public IHttpActionResult Put(stuff st)
+        {
+            var updatestuff = DC.stuffs.Where(x => x.itemid == st.itemid).FirstOrDefault<stuff>();
+            if (updatestuff != null)
+            {
+                updatestuff.itemid = st.itemid;
+                updatestuff.name = st.name;
+                updatestuff.descriptions = st.descriptions;
+                updatestuff.price = st.price;
+                DC.SaveChanges();
+            }
+            else
+            {
+                return NotFound();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            var stuffdel = DC.stuffs.Where(x => x.itemid == id).FirstOrDefault();
+            DC.Entry(stuffdel).State = System.Data.Entity.EntityState.Deleted;
+            DC.SaveChanges();
+            return Ok();
+
+        }
+
+
+    }
+}
---
+++ Controllers/OperationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using Application.Models;
+
+namespace Application.Controllers
+{
+    public class OperationsController : Controller
+    {
+        // GET: Operations
+        public ActionResult Index()
+        {
+            IEnumerable<stuff> stuffobj = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri(" http://localhost:65460/api/Inventory");
+            var consumeapi = hc.GetAsync("Inventory");
+            consumeapi.Wait();
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                var displaydata = readdata.Content.ReadAsAsync<IList<stuff>>();
+                displaydata.Wait();
+                stuffobj = displaydata.Result;
+
+
+            }
+
+            return View(stuffobj);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(stuff insertstuff)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:65460/api/Inventory");
+            var insertrecord = hc.PostAsJsonAsync<stuff>("Inventory", insertstuff);
+            insertrecord.Wait();
+            var savedata = insertrecord.Result;
+            if (savedata.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View("Create");
+        }
+
+
+
+
+
+        public ActionResult Details(int id)
+        {
+            stuff stuffobj = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:65460/api/");
+            var consumeapi = hc.GetAsync("Inventory?id=" + id.ToString());
+            consumeapi.Wait();
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                var displaydata = readdata.Content.ReadAsAsync<stuff>();
+                displaydata.Wait();
+                stuffobj = displaydata.Result;
+
+            }
+
+            return View(stuffobj);
+
+        }
+        public ActionResult Edit(int id)
+        {
+            stuff stuffobj = null;
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:65460/api/Inventory");
+            var consumeapi = hc.GetAsync("Inventory?id=" + id.ToString());
+            consumeapi.Wait();
+            var readdata = consumeapi.Result;
+            if (readdata.IsSuccessStatusCode)
+            {
+                var displaydata = readdata.Content.ReadAsAsync<stuff>();
+                displaydata.Wait();
+                stuffobj = displaydata.Result;
+
+            }
+
+            return View(stuffobj);
+
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(stuff st)
+        {
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:65460/api/Inventory");
+            var insertrecord = hc.PutAsJsonAsync<stuff>("Inventory", st);
+            insertrecord.Wait();
+            var savedata = insertrecord.Result;
+            if (savedata.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            else
+            {
+                ViewBag.message = "not updated";
+            }
+            return View(st);
+
+        }
+
+        public ActionResult Delete(int id)
+        {
+
+            HttpClient hc = new HttpClient();
+            hc.BaseAddress = new Uri("http://localhost:65460/api/Inventory");
+            var delrecord = hc.DeleteAsync("Inventory/" + id.ToString());
+            delrecord.Wait();
+            var displaydata = delrecord.Result;
+            if (displaydata.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View("Index");
+        }
+
+    }
+}
