673d3be78432f0a3ed7b05df75ef832b8e3e9591
Chetan538 <82130147+Chetan538@users.noreply.github.com>
2021-04-12 10:49:13 +0530

    Add files via upload

---
+++ Models/Stuffclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Application.Models
+{
+    public class Stuffclass
+    {
+        public int itemid { get; set; }
+        public string name { get; set; }
+        public string descriptions { get; set; }
+        public Nullable<int> price { get; set; }
+    }
+}
